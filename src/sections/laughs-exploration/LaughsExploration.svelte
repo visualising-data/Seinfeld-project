dc0c0c45fe8e5e2d5e873b6222b1b27a7bb3e7ac
Anne-Marie Dufour <annema.dufour@gmail.com>
2025-10-21 12:34:38 +0200

    Text update

--- src/sections/laughs-exploration/LaughsExploration.svelte
+++ src/sections/laughs-exploration/LaughsExploration.svelte
@@ -6,7 +6,7 @@
   <div class="container">
     <!-- Header -->
     <div class="mb-8">
-      <h3>“It was the Colonel Mustard… with the Chandelier… in the Kitchen!”</h3>
+      <h3>“It was Colonel Mustard… with the Candlestick… in the Kitchen!”</h3>
       <div style="max-width: 900px;">Like in a game of Cluedo, combinations of lead characters, supporting characters, and locations were used to generate laughs.</div>
     </div>
   </div>
