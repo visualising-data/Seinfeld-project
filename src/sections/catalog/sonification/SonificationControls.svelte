9e851592833b1bb6350e895e14df07ddb0db383f
Anne-Marie Dufour <annema.dufour@gmail.com>
2026-01-05 11:54:34 +0100

    Bring actions together

--- src/sections/catalog/sonification/SonificationControls.svelte
+++ src/sections/catalog/sonification/SonificationControls.svelte
@@ -24,55 +24,59 @@
   class="relative"
   style="max-width: {innerWidth >= 1280 ? scenesWidth : innerWidth - 75}px; margin-top: -4px;"
 >
-  <div
-    class="small absolute left-0 pointer-events-none"
-    style="margin-top: {innerWidth >= 1280 ? 0 : 16}px; top: {innerWidth >= 1280
-      ? 0
-      : -67}px; line-height: 24px; color: {$soundIsAuth ? '#E71D80' : '#BEBABC'};"
-  >
-    Listen to this episode's data
-  </div>
-  <div class="mr-8 mt-2 flex justify-start lg:justify-center">
-    <button
-      class="mx-2"
-      onclick={playPrev}
-      disabled={!isPlaying || !$soundIsAuth || playingScene <= 1}
-      style="cursor: {$soundIsAuth ? 'pointer' : 'not-allowed'};"
-    >
-      <Prev
-        color={isPlaying && $soundIsAuth && playingScene > 1 ? '#E71D80' : '#BEBABC'}
-        size="md"
-      />
-    </button>
-    {#if isPlaying}
+  <div class="mr-8 mt-2 flex flex-col items-start lg:items-center">
+    <div class="flex items-center">
       <button
         class="mx-2"
-        onclick={stop}
+        onclick={playPrev}
+        disabled={!isPlaying || !$soundIsAuth || playingScene <= 1}
         style="cursor: {$soundIsAuth ? 'pointer' : 'not-allowed'};"
       >
-        <StopIcon color="#E71D80" />
+        <Prev
+          color={isPlaying && $soundIsAuth && playingScene > 1 ? '#E71D80' : '#BEBABC'}
+          size="md"
+        />
       </button>
-    {:else}
+      <div class="flex mx-1">
+        {#if isPlaying}
+          <button
+            class="mx-2"
+            onclick={stop}
+            style="cursor: {$soundIsAuth ? 'pointer' : 'not-allowed'};"
+          >
+            <StopIcon color="#E71D80" />
+          </button>
+        {:else}
+          <button
+            class="mx-2"
+            onclick={play}
+            disabled={!$soundIsAuth}
+            style="cursor: {$soundIsAuth ? 'pointer' : 'not-allowed'};"
+          >
+            <PlayIcon color={$soundIsAuth ? '#E71D80' : '#BEBABC'} />
+          </button>
+        {/if}
+      </div>
       <button
         class="mx-2"
-        onclick={play}
-        disabled={!$soundIsAuth}
+        onclick={playNext}
+        disabled={!isPlaying || !$soundIsAuth || playingScene == numScenes}
         style="cursor: {$soundIsAuth ? 'pointer' : 'not-allowed'};"
       >
-        <PlayIcon color={$soundIsAuth ? '#E71D80' : '#BEBABC'} />
+        <Next
+          color={isPlaying && $soundIsAuth && playingScene < numScenes ? '#E71D80' : '#BEBABC'}
+          size="md"
+        />
       </button>
-    {/if}
-    <button
-      class="mx-2"
-      onclick={playNext}
-      disabled={!isPlaying || !$soundIsAuth || playingScene == numScenes}
-      style="cursor: {$soundIsAuth ? 'pointer' : 'not-allowed'};"
+    </div>
+    <div
+      class="small pointer-events-none"
+      style="margin-top: {innerWidth >= 1280 ? 0 : 16}px; top: {innerWidth >= 1280
+        ? 0
+        : -67}px; line-height: 24px; color: {$soundIsAuth ? '#E71D80' : '#BEBABC'};"
     >
-      <Next
-        color={isPlaying && $soundIsAuth && playingScene < numScenes ? '#E71D80' : '#BEBABC'}
-        size="md"
-      />
-    </button>
+      Listen to this episode's data
+    </div>
   </div>
   <div class="absolute right-0 top-1">
     <button onclick={toggleSonificationLegend}><InfoIcon /></button>
