ab6a6f7bf6dcbba16b7f345c9561a372637a2dee
Anne-Marie Dufour <annema.dufour@gmail.com>
2026-02-21 17:20:42 +0100

    Display location icons randomly in section intro

--- src/sections/locations/Intro.svelte
+++ src/sections/locations/Intro.svelte
@@ -1,20 +1,19 @@
 <script>
   import { onMount } from 'svelte';
-	import { gsap } from 'gsap/dist/gsap';
-	import Lenis from 'lenis';
-	import { soundIsAuth } from '../../stores/soundAuthStore';
-	import tv_noise from '$lib/assets/tv_noise.png';
-  import { locations } from '$lib/data/locations';
-  import { getLocationIconPath } from '../../utils/getLocationIconPath';
+  import { gsap } from 'gsap/dist/gsap';
+  import Lenis from 'lenis';
+  import { soundIsAuth } from '../../stores/soundAuthStore';
+  import tv_noise from '$lib/assets/tv_noise.png';
+  import LocationsScreen3 from './LocationsScreen3.svelte';
 
   onMount(() => {
-    gsap.set('#locations p, #locations .supporting-char', { translateY: 100, opacity: 0 });
+    gsap.set('#locations p', { translateY: 100, opacity: 0 });
 
     const tl1 = gsap.timeline({
       scrollTrigger: {
         trigger: '#locations_screen_1',
-        start: 'top center'
-      }
+        start: 'top center',
+      },
     });
     tl1
       .to('#locations_screen_1 p', {
@@ -22,107 +21,86 @@
         opacity: 1,
         duration: 1,
         ease: 'power3.out',
-        stagger: { each: 0.3 }
+        stagger: { each: 0.3 },
       })
-      .to('#locations_screen_1 .highlight', {
-        webkitTextFillColor: 'transparent',
-        backgroundPosition: '200% center',
-        duration: 2,
-        delay: 1,
-        ease: 'power3.out'
-      }, '<-0.5');
+      .to(
+        '#locations_screen_1 .highlight',
+        {
+          webkitTextFillColor: 'transparent',
+          backgroundPosition: '200% center',
+          duration: 2,
+          delay: 1,
+          ease: 'power3.out',
+        },
+        '<-0.5',
+      );
 
     const tl2 = gsap.timeline({
       scrollTrigger: {
         trigger: '#locations_screen_2',
-        start: 'top top<20%'
-      }
+        start: 'top top<20%',
+      },
     });
     tl2
       .to('#locations_screen_2 p', {
         translateY: 0,
         opacity: 1,
         duration: 1,
-        ease: 'power3.out'
-      })
-      .to('#locations_screen_2 .highlight', {
-        webkitTextFillColor: 'transparent',
-        backgroundPosition: '200% center',
-        duration: 2,
-        delay: 1,
-        ease: 'power3.out'
-      }, '<-0.5');
-
-    const tl3 = gsap.timeline({
-      scrollTrigger: {
-        trigger: '#locations_screen_3',
-        start: 'top center'
-      }
-    });
-    tl3
-      .to('#locations_screen_3 p', {
-        translateY: 0,
-        opacity: 1,
-        duration: 1,
         ease: 'power3.out',
-        stagger: { each: 0.3 }
       })
-      .to('#locations_screen_3 .highlight', {
-        webkitTextFillColor: 'transparent',
-        backgroundPosition: '200% center',
-        duration: 2,
-        delay: 1,
-        ease: 'power3.out'
-      }, '<-0.5')
-      .to('#locations_screen_3 .supporting-char', {
-        translateY: 0,
-        opacity: 1,
-        duration: 0.7,
-        ease: 'power3.out',
-        stagger: { each: 0.1, from: 'random' }
-      }, '<-0.5');
+      .to(
+        '#locations_screen_2 .highlight',
+        {
+          webkitTextFillColor: 'transparent',
+          backgroundPosition: '200% center',
+          duration: 2,
+          delay: 1,
+          ease: 'power3.out',
+        },
+        '<-0.5',
+      );
 
     // Add parallax effect to videos
-		let videos = gsap.utils.toArray('.parallax');
-		videos.forEach((video) => {
-			const speed = video.dataset.speed;
-			gsap.to(video, {
-				yPercent: speed * 50,
-				ease: 'none',
-				scrollTrigger: {
-					trigger: video,
-					start: 'top bottom',
-					scrub: true
-				}
-			});
-		});
+    let videos = gsap.utils.toArray('.parallax');
+    videos.forEach((video) => {
+      const speed = video.dataset.speed;
+      gsap.to(video, {
+        yPercent: speed * 50,
+        ease: 'none',
+        scrollTrigger: {
+          trigger: video,
+          start: 'top bottom',
+          scrub: true,
+        },
+      });
+    });
 
-		// Smooth scroll
-		const lenis = new Lenis();
+    // Smooth scroll
+    const lenis = new Lenis();
 
-		/**
-		 * @param {number} time
-		 */
-		function raf(time) {
-			lenis.raf(time);
-			requestAnimationFrame(raf);
-		}
+    /**
+     * @param {number} time
+     */
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-		requestAnimationFrame(raf);
+    requestAnimationFrame(raf);
   });
 
   let video1IsMuted = $state(true);
   let video2IsMuted = $state(true);
   const handleVideoMouseEnter = (/** @type {number} */ video) => {
     if ($soundIsAuth) {
-			video1IsMuted = video === 1 ? false : true;
-			video2IsMuted = video === 2 ? false : true;
-		}
-	};
-	const handleVideoMouseLeave = (/** @type {{ target: any; }} */ e) => {
-		video1IsMuted = true;
-		video2IsMuted = true;
-	};
+      video1IsMuted = video === 1 ? false : true;
+      video2IsMuted = video === 2 ? false : true;
+    }
+  };
+  const handleVideoMouseLeave = (/** @type {{ target: any; }} */ e) => {
+    video1IsMuted = true;
+    video2IsMuted = true;
+  };
 </script>
 
 <div id="locations" class="bg-black text-white">
@@ -131,8 +109,19 @@
     <div class="container">
       <div class="grid grid-cols-12 gap-4">
         <div class="col-span-12 md:col-span-7 h-screen flex flex-col justify-center">
-          <p>After examining the use of lead and supporting characters - <span class="highlight">the who</span> of the show - the next dimension that has an influence on the texture of a sitcom relates to the scene locations - <span class="highlight">the where</span> of the show.</p>
-          <p>Classifying locations concerns defining the characteristics and use of different settings as the physical backdrop to each scene. Sometimes scenes were set indoors, sometimes outdoors: sometimes scenes were shot in a studio, sometimes they were shot on location.</p>
+          <p>
+            After examining the use of lead and supporting characters - <span class="highlight"
+              >the who</span
+            >
+            of the show - the next dimension that has an influence on the texture of a sitcom relates
+            to the scene locations - <span class="highlight">the where</span> of the show.
+          </p>
+          <p>
+            Classifying locations concerns defining the characteristics and use of different
+            settings as the physical backdrop to each scene. Sometimes scenes were set indoors,
+            sometimes outdoors: sometimes scenes were shot in a studio, sometimes they were shot on
+            location.
+          </p>
         </div>
       </div>
     </div>
@@ -143,8 +132,20 @@
     <div class="container">
       <div class="grid grid-cols-12 gap-4">
         <div class="col-span-12 md:col-span-7 md:h-screen flex flex-col justify-center">
-          <p>Just as the list of characters grew, so too did the spatial ambitions of the storylines. Once the show became a fixture in NBC's Fall schedule, from Season Three onwards, it gained a significant production budget increase. </p>
-          <p>As Jennifer Keishin Armstrong noted in <a href="https://jenniferkarmstrong.com/books/seinfeldia/" target="_blank">her book</a>, S3E6 "The Parking Garage" marked the first episode in which an outside custom set, away from a studio audience, was built and used. This began a pattern of filming on-location and off-set that gradually expanded the physicality of the Seinfeld universe.</p>
+          <p>
+            Just as the list of characters grew, so too did the spatial ambitions of the storylines.
+            Once the show became a fixture in NBC's Fall schedule, from Season Three onwards, it
+            gained a significant production budget increase.
+          </p>
+          <p>
+            As Jennifer Keishin Armstrong noted in <a
+              href="https://jenniferkarmstrong.com/books/seinfeldia/"
+              target="_blank">her book</a
+            >, S3E6 "The Parking Garage" marked the first episode in which an outside custom set,
+            away from a studio audience, was built and used. This began a pattern of filming
+            on-location and off-set that gradually expanded the physicality of the Seinfeld
+            universe.
+          </p>
         </div>
         <div class="col-span-12 md:col-span-5">
           <div class="grid grid-cols-5 mt-16">
@@ -203,47 +204,27 @@
   </div>
 
   <!-- Screen 3 -->
-  <div id="locations_screen_3" class="md:h-screen w-screen py-60 md:py-0">
-    <div class="container">
-      <div class="grid grid-cols-12 md:gap-20">
-        <div class="col-span-12 md:col-span-7 md:h-screen md:flex md:flex-col md:justify-center">
-          <p>As with the task of classifying characters, there were challenges with establishing a robust taxonomy for distinguishing different types of locations. You don't want too many distinct groups, but you want enough to capture the variety of different physical settings.</p>
-          <p>The final list of distinct locations would include the homes of the four leads, as well as a further 'family home' to cover all and any family setting. The 'diner' was another common hangout location, but separate from other 'places of leisure', which would include other restaurants as well as cinemas and indoor sports venues. The lead characters' 'workplace' would be a recurring setting, from Jerry doing his stand-up routines to George working at Yankee Stadium. Scenes set in various forms of 'transport', like taxis, planes, or cars, were common, as were 'outside' locations like visits to the Hamptons or when disputes arose about street-side parking. An 'other' grouping would be necessary to organise together the many other residual additional types of locations used.</p>
-        </div>
-        <div class="col-span-12 md:col-span-4 flex items-center">
-          <div class="grid grid-cols-2 gap-8 flex-wrap">
-            {#each locations as location}
-              <div class="supporting-char flex flex-col items-center">
-                <div class="character rounded-full bg-contain bg-center" 
-                     style="background-image: url('{getLocationIconPath(location.id)}'); width: 75px; height: 75px;"></div>
-                <div>{location.label}</div>
-              </div>
-            {/each}
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <LocationsScreen3 />
 </div>
 
 <style>
-	.parallax {
-		box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-	}
-	.episode {
-		position: absolute;
-		z-index: 2;
-		left: 5px;
-		bottom: 0;
-		font-size: 1.125rem;
-		line-height: 1.2;
-		font-weight: 600;
-		transform: translateY(15px);
-		opacity: 0;
-		transition: all 350ms cubic-bezier(0.165, 0.84, 0.44, 1);
-	}
-	.parallax:hover .episode {
-		transform: translateY(0);
-		opacity: 1;
-	}
+  .parallax {
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  }
+  .episode {
+    position: absolute;
+    z-index: 2;
+    left: 5px;
+    bottom: 0;
+    font-size: 1.125rem;
+    line-height: 1.2;
+    font-weight: 600;
+    transform: translateY(15px);
+    opacity: 0;
+    transition: all 350ms cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+  .parallax:hover .episode {
+    transform: translateY(0);
+    opacity: 1;
+  }
 </style>
---
+++ src/sections/locations/LocationsScreen3.svelte
@@ -0,0 +1,248 @@
+<script>
+  import { onMount } from 'svelte';
+  import { gsap } from 'gsap/dist/gsap';
+  import { locations } from '$lib/data/locations';
+  import { getLocationIconPath } from '../../utils/getLocationIconPath';
+  import { forceSimulation, forceCollide, forceManyBody } from 'd3-force';
+
+  let locationPositions = $state(/** @type {{ top: number; left: number }[]} */ ([]));
+  let iconsContainer = /** @type {HTMLDivElement} */ (/** @type {unknown} */ (null));
+
+  onMount(() => {
+    gsap.set('#locations_screen_3 .supporting-char', { translateY: 100, opacity: 0 });
+
+    const tl3 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#locations_screen_3',
+        start: 'top center',
+      },
+      onComplete: () => {
+        gsap.utils.toArray('#locations_screen_3 .supporting-char').forEach((el) => {
+          gsap.to(el, {
+            y: -(Math.random() * 3 + 3), // 3–6px upward
+            duration: 2.5 + Math.random() * 1.5, // 2.5–4s per half-cycle
+            ease: 'sine.inOut',
+            yoyo: true,
+            repeat: -1,
+            delay: Math.random() * 2,
+          });
+        });
+      },
+    });
+    tl3
+      .to('#locations_screen_3 p', {
+        translateY: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power3.out',
+        stagger: { each: 0.3 },
+      })
+      .to(
+        '#locations_screen_3 .highlight',
+        {
+          webkitTextFillColor: 'transparent',
+          backgroundPosition: '200% center',
+          duration: 2,
+          delay: 1,
+          ease: 'power3.out',
+        },
+        '<-0.5',
+      )
+      .to(
+        '#locations_screen_3 .supporting-char',
+        {
+          translateY: 0,
+          opacity: 1,
+          duration: 1.2,
+          ease: 'power3.out',
+          stagger: { each: 0.2, from: 'random' },
+        },
+        '<-0.5',
+      );
+
+    function computePositions() {
+      const screenW = iconsContainer.clientWidth;
+      const screenH = iconsContainer.clientHeight;
+      const border = 30;
+      const xMin = border + 37.5;
+      const xMax = screenW - border - 37.5;
+      const yMin = border + 37.5;
+      const yMax = screenH - border - 67.5;
+
+      const textCol = iconsContainer.querySelector('.locations-text-col');
+      const screenRect = iconsContainer.getBoundingClientRect();
+      const textRect = textCol.getBoundingClientRect();
+      const textZone = {
+        x1: textRect.left - screenRect.left - border,
+        x2: textRect.right - screenRect.left + border,
+        y1: textRect.top - screenRect.top - border,
+        y2: textRect.bottom - screenRect.top + border,
+      };
+
+      // Disc centred in the available area (right of text zone)
+      const cx = (textZone.x2 + screenW) / 2;
+      const cy = screenH / 2;
+      const discRadius = Math.min((screenW - textZone.x2) * 0.3, screenH * 0.28);
+
+      // Measure each element's real rendered dimensions for accurate collision
+      const iconEls = Array.from(iconsContainer.querySelectorAll('.supporting-char'));
+
+      // Seed nodes uniformly within the disc using polar coordinates
+      const nodes = locations.map((_, i) => {
+        const el = /** @type {HTMLElement | undefined} */ (iconEls[i]);
+        const w = el ? el.offsetWidth : 75;
+        const h = el ? el.offsetHeight : 100;
+        const angle = Math.random() * 2 * Math.PI;
+        const r = Math.sqrt(Math.random()) * discRadius;
+        return {
+          x: Math.max(xMin, Math.min(cx + r * Math.cos(angle), xMax)),
+          y: Math.max(yMin, Math.min(cy + r * Math.sin(angle), yMax)),
+          collisionR: Math.max(w, h) / 2 + 5,
+          w,
+          h,
+        };
+      });
+
+      // Custom force: push nodes back inside the disc if they drift outside
+      function forceBoundCircle(centerX, centerY, r) {
+        /** @type {any[]} */ let simNodes;
+        /** @param {number} alpha */
+        function force(alpha) {
+          for (const node of simNodes) {
+            const dx = node.x - centerX;
+            const dy = node.y - centerY;
+            const d = Math.sqrt(dx * dx + dy * dy);
+            if (d > r) {
+              const k = (d - r) / d * alpha * 2;
+              node.vx -= dx * k;
+              node.vy -= dy * k;
+            }
+          }
+        }
+        force.initialize = (/** @type {any[]} */ n) => { simNodes = n; };
+        return force;
+      }
+
+      forceSimulation(nodes)
+        .force('collide', forceCollide(/** @type {any} */ ((d) => d.collisionR)).strength(1).iterations(4))
+        .force('charge', forceManyBody().strength(-20))
+        .force('bound', forceBoundCircle(cx, cy, discRadius))
+        .stop()
+        .tick(400);
+
+      locationPositions = nodes.map((node) => ({
+        top: Math.max(border, Math.min(node.y - node.h / 2, screenH - node.h - border)),
+        left: Math.max(border, Math.min(node.x - node.w / 2, screenW - node.w - border)),
+      }));
+    }
+
+    computePositions();
+
+    let rafId = 0;
+    const observer = new ResizeObserver(() => {
+      cancelAnimationFrame(rafId);
+      rafId = requestAnimationFrame(computePositions);
+    });
+    observer.observe(iconsContainer);
+
+    return () => {
+      observer.disconnect();
+      cancelAnimationFrame(rafId);
+    };
+  });
+
+  const handleIconEnter = (/** @type {number} */ index) => {
+    const wrappers = Array.from(iconsContainer.querySelectorAll('.icon-wrapper'));
+    const chars = Array.from(iconsContainer.querySelectorAll('.supporting-char'));
+
+    // Scale up the hovered icon
+    gsap.to(chars[index], { scale: 1.3, duration: 0.3, ease: 'back.out(1.7)' });
+
+    // Measure the hovered icon's centre in viewport coordinates
+    const hoveredRect = wrappers[index].getBoundingClientRect();
+    const hoveredCx = hoveredRect.left + hoveredRect.width / 2;
+    const hoveredCy = hoveredRect.top + hoveredRect.height / 2;
+
+    // Push nearby wrappers away
+    wrappers.forEach((wrapper, i) => {
+      if (i === index) return;
+      const rect = wrapper.getBoundingClientRect();
+      const dx = rect.left + rect.width / 2 - hoveredCx;
+      const dy = rect.top + rect.height / 2 - hoveredCy;
+      const dist = Math.sqrt(dx * dx + dy * dy);
+      const maxDist = 220;
+      if (dist < maxDist) {
+        const force = Math.pow((maxDist - dist) / maxDist, 1.5) * 35;
+        gsap.to(wrapper, {
+          x: (dx / dist) * force,
+          y: (dy / dist) * force,
+          duration: 0.5,
+          ease: 'power2.out',
+        });
+      }
+    });
+  };
+
+  const handleIconLeave = (/** @type {number} */ index) => {
+    const wrappers = Array.from(iconsContainer.querySelectorAll('.icon-wrapper'));
+    const chars = Array.from(iconsContainer.querySelectorAll('.supporting-char'));
+
+    // Scale the hovered icon back
+    gsap.to(chars[index], { scale: 1, duration: 0.4, ease: 'power2.inOut' });
+
+    // Spring all wrappers back to their original positions
+    wrappers.forEach((wrapper, i) => {
+      if (i === index) return;
+      gsap.to(wrapper, { x: 0, y: 0, duration: 0.6, ease: 'elastic.out(1, 0.5)' });
+    });
+  };
+</script>
+
+<div id="locations_screen_3" class="md:h-screen w-screen py-60 md:py-0 relative" bind:this={iconsContainer}>
+  <div class="container">
+    <div class="grid grid-cols-12 md:gap-20">
+      <div class="locations-text-col col-span-12 md:col-span-7 md:h-screen md:flex md:flex-col md:justify-center">
+        <p>
+          As with the task of classifying characters, there were challenges with establishing a
+          robust taxonomy for distinguishing different types of locations. You don't want too many
+          distinct groups, but you want enough to capture the variety of different physical
+          settings.
+        </p>
+        <p>
+          The final list of distinct locations would include the homes of the four leads, as well
+          as a further 'family home' to cover all and any family setting. The 'diner' was another
+          common hangout location, but separate from other 'places of leisure', which would
+          include other restaurants as well as cinemas and indoor sports venues. The lead
+          characters' 'workplace' would be a recurring setting, from Jerry doing his stand-up
+          routines to George working at Yankee Stadium. Scenes set in various forms of
+          'transport', like taxis, planes, or cars, were common, as were 'outside' locations like
+          visits to the Hamptons or when disputes arose about street-side parking. An 'other'
+          grouping would be necessary to organise together the many other residual additional
+          types of locations used.
+        </p>
+      </div>
+      <div class="col-span-12 md:col-span-4">
+        {#each locations as location, i}
+          <div
+            class="icon-wrapper absolute cursor-pointer"
+            style="top: {locationPositions[i]?.top ?? 0}px; left: {locationPositions[i]?.left ?? 0}px;"
+            role="button"
+            tabindex="0"
+            onmouseenter={() => handleIconEnter(i)}
+            onmouseleave={() => handleIconLeave(i)}
+          >
+            <div class="supporting-char flex flex-col items-center">
+              <div
+                class="character rounded-full bg-contain bg-center"
+                style="background-color: #12020A; background-image: url('{getLocationIconPath(
+                  location.id,
+                )}'); width: 75px; height: 75px;"
+              ></div>
+              <div class="mid pt-2">{location.label}</div>
+            </div>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </div>
+</div>
