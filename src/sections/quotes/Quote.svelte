2515d099d7677ea30339fe885e5aea3cae9a1eb0
Anne-Marie Dufour <annema.dufour@gmail.com>
2026-03-03 11:00:20 +0100

    Remove audio from quotes sections

--- src/sections/quotes/Quote.svelte
+++ src/sections/quotes/Quote.svelte
@@ -1,91 +1,40 @@
 <script>
-  import * as Tone from 'tone';
-  import { seasons } from "$lib/data/seasons";
-  import PlayQuoteIcon from "./PlayQuoteIcon.svelte";
+  import { seasons } from '$lib/data/seasons';
 
-  let { quote } = $props()
-
-  let isQuoteHovered = $state(false)
-  let isQuotePlaying = $state(false)
-
-  /**
-	 * @type {Tone.Player}
-	 */
-  let audio
-  const playQuote = () => {
-		isQuotePlaying = true
-    // @ts-ignore
-		audio = new Tone.Player().toDestination()
-    audio.load(`https://amdufour.github.io/hosted-data/apis/audio_quotes/${quote.audio_clip_id}.m4a`).then(() => {
-      audio.fadeIn = 1
-      audio.fadeOut = 1
-      audio.start()
-    });
-
-		setTimeout(
-			() => {
-				isQuotePlaying = false
-			},
-			(quote.duration + 1) * 1000
-		);
-	};
-  const stopQuote = () => {
-    audio.stop()
-    isQuotePlaying = false
-  }
-
-	const handleMouseEnter = (/** @type {number} */ id) => {
-		isQuoteHovered = true
-	}
-	const handleMouseLeave = () => {
-		isQuoteHovered = false
-	}
+  let { quote } = $props();
 </script>
 
-<li 
-	class="flex whitespace-nowrap quote quote-${quote.audio_clip_id}"
-	onmouseenter={() => handleMouseEnter(quote.audio_clip_id)}
-	onmouseleave={handleMouseLeave}
->
-  <button class="flex cursor-pointer" onclick={isQuotePlaying ? stopQuote : playQuote}>
-    <div class="pr-2 pt-2 lg:pt-3 flex justify-end" style={"width: 54px;"}>
-      {#if isQuoteHovered}
-        <PlayQuoteIcon isPlaying={isQuotePlaying} />
-      {/if}
-    </div>
-    <div
-      class={`quote quote-${quote.audio_clip_id} relative inline ${isQuotePlaying ? 'playing' : ''}`}
-      style={`color: ${seasons.find((s) => s.seasonNum === quote.season)?.accessibleOverDarkColor}`}
+<li class="flex mx-6 whitespace-nowrap quote quote-${quote.audio_clip_id}">
+  <div
+    class={`quote quote-${quote.audio_clip_id} relative inline`}
+    style={`color: ${seasons.find((s) => s.seasonNum === quote.season)?.accessibleOverDarkColor}`}
+  >
+    {#if quote.revised_quote_text.length > 0}
+      {quote.revised_quote_text}
+    {:else}
+      {quote.quote}
+    {/if}
+    <span class="quote-info small absolute left-0 top-9 lg:top-11" role="contentinfo"
+      >{`s${quote.season}e${quote.episode} ${quote.episode_title}, ${quote.who}`}</span
     >
-      {#if quote.revised_quote_text.length > 0}
-        {quote.revised_quote_text}
-      {:else}
-        {quote.quote}
-      {/if}
-      <span
-        class="quote-info small absolute left-0 top-9 lg:top-11"
-        role="contentinfo"
-        >{`s${quote.season}e${quote.episode} ${quote.episode_title}, ${quote.who}`}</span
-      >
-    </div>
-  </button>
+  </div>
 </li>
 
 <style>
   .quote {
-		transition: color 250ms ease-out;
-	}
-	.quote.playing {
-		color: #e71d80 !important;
-	}
-	.quote-info {
-		transform: translateY(-10px);
-		opacity: 0;
-		transition: all 250ms ease-out;
-	}
-	.quote:hover .quote-info,
-	.quote.playing .quote-info {
-		transform: translateY(0);
-		opacity: 1;
-	}
+    transition: color 250ms ease-out;
+  }
+  .quote.playing {
+    color: #e71d80 !important;
+  }
+  .quote-info {
+    transform: translateY(-10px);
+    opacity: 0;
+    transition: all 250ms ease-out;
+  }
+  .quote:hover .quote-info,
+  .quote.playing .quote-info {
+    transform: translateY(0);
+    opacity: 1;
+  }
 </style>
