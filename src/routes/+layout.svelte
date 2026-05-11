e77d568c2435c4c84ccf1a7060b91ca9841433f6
Anne-Marie Dufour <annema.dufour@gmail.com>
2026-05-11 11:36:47 +0200

    Add og images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
   <!-- Open Graph (social sharing) -->
   <meta property="og:title" content="The Seinfeld Chronicles" />
   <meta property="og:description" content="An unnecessary data exploration" />
-  <meta property="og:image" content="https://yoursite.com/og-image.png" />
+  <meta property="og:image" content="https://yoursite.com/og-image.jpg" />
   <meta property="og:url" content="https://yoursite.com" />
   <meta property="og:type" content="website" />
 
@@ -18,7 +18,7 @@
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content="The Seinfeld Chronicles" />
   <meta name="twitter:description" content="An unnecessary data exploration" />
-  <meta name="twitter:image" content="https://yoursite.com/og-image.png" />
+  <meta name="twitter:image" content="https://yoursite.com/og-image-twitter.jpg" />
 </svelte:head>
 
 {@render children()}
