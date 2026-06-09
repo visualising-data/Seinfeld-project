0e108788d55f2070a5edee00385b18eff66c151f
Anne-Marie Dufour <annema.dufour@gmail.com>
2026-06-09 16:43:30 +0200

    Add meta description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,22 +5,31 @@
 
 <svelte:head>
   <title>The Seinfeld Chronicles</title>
-  <meta name="description" content="An unnecessary data exploration" />
+  <meta
+    name="description"
+    content="The Seinfeld Chronicles digital edition visually and sonically presents an entirely unnecessary data-driven exploration of every episode of Seinfeld."
+  />
 
   <!-- Open Graph (social sharing) -->
   <meta property="og:title" content="The Seinfeld Chronicles" />
-  <meta property="og:description" content="An unnecessary data exploration" />
+  <meta
+    property="og:description"
+    content="The Seinfeld Chronicles digital edition visually and sonically presents an entirely unnecessary data-driven exploration of every episode of Seinfeld."
+  />
   <meta
     property="og:image"
     content="https://amdufour.github.io/hosted-data/apis/images/og-image.jpg"
   />
-  <meta property="og:url" content="https://yoursite.com" />
+  <meta property="og:url" content="https://seinfeld.visualisingdata.com/" />
   <meta property="og:type" content="website" />
 
   <!-- Twitter/X Card -->
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content="The Seinfeld Chronicles" />
-  <meta name="twitter:description" content="An unnecessary data exploration" />
+  <meta
+    name="twitter:description"
+    content="The Seinfeld Chronicles digital edition visually and sonically presents an entirely unnecessary data-driven exploration of every episode of Seinfeld."
+  />
   <meta
     name="twitter:image"
     content="https://amdufour.github.io/hosted-data/apis/images/og-image-twitter.jpg"
